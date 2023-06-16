bd1f96d821e63c8bb6a79669dfc2be6bf693c809
JohnKeeney <john.keeney@est.tech>
2023-06-16 02:05:37 +0100

    Roll version after release nonrtric-plt-helmmanager:1.3.1
    Issue-ID: NONRTRIC-883
    Change-Id: Ia9b4cb9e9a73011aeb1b0cd5dc1872d89dde03f7
    Signed-off-by: JohnKeeney <john.keeney@est.tech>

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,7 +19,7 @@ from docs_conf.conf import *
 
 #branch configuration
 
-branch = 'latest'
+branch = 'h-release'
 
 linkcheck_ignore = [
     'http://localhost.*',
